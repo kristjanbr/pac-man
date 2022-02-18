b1b44cad7294c77df9e111f5ccb623208a63259b
kristjTSC <kristjanbratasevec1@gmail.com>
2022-02-18 07:44:01 +0100

    Major update 1
    Added JSON, 3 mazes, fruit generation, etc.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+/*----CANVAS INIT.------*/
+var canvas = document.getElementById('canvasMaze');
+var ctx = canvas.getContext('2d');
+ctx.scale(2, 2);
+
 var dotCanvas = document.getElementById('pacDots');
 var dctx = dotCanvas.getContext('2d');
 dctx.scale(2, 2);
 
+var fruitCanvas = document.getElementById('fruity');
+var fctx = fruitCanvas.getContext('2d');
+fctx.scale(2, 2);
+
+var movCanvas = document.getElementById('moving');
+var mctx = movCanvas.getContext('2d');
+mctx.scale(2, 2);
+
+/*----LOCALSTORAGE.------*/
 if (localStorage.getItem("tokens") == null) {
     localStorage.setItem("tokens", "1");
 }
@@ -10,41 +24,98 @@ var tokens = parseInt(localStorage.getItem("tokens"));
 var playbtn = document.getElementById('playbtn');
 
 var coins = document.getElementById('coins');
-var grm="TOKENS";
-    if(tokens==1)
-        grm="TOKEN"
-    coins.innerHTML="YOU HAVE: "+tokens+" "+grm+"!";
-
-var movCanvas = document.getElementById('moving');
-var mctx = movCanvas.getContext('2d');
-
-mctx.scale(2, 2);
-var img = new Image();
-img.src = 'img/pacman.png';
+coins.innerHTML = "No. of tokens: " + tokens;
 
 if (tokens <= 0) {
     playbtn.disabled = true;
     playbtn.className = "nes-btn is-disabled";
 }
 
+/*----IMAGES INIT.------*/
+var img = new Image();
+img.src = 'img/pacman.png';
+var f_img = new Image();
+f_img.src = 'img/fruits.png';
+
+
+randmaze = Math.floor(Math.random() * 3);
+localStorage.setItem("mazeno", randmaze.toString());
+/*
+a();
+async function a() {
+    const response = await fetch("js/mazes.json");
+    const data = await response.json();
+    tab = data[1].path;
+    spawnDotsa(18, tab);
+}
+async function spawnDotsa(count, tab) {
+    dctx.clearRect(0, 0, dotCanvas.width, dotCanvas.height);
+    dctx.fillStyle = "#f6e58d";
+    for (var x = 1; x <= count; x++) {
+        for (var y = 1; y <= count; y++) {
+            if (tab[y][x] != 0) {
+                dctx.beginPath();
+                dctx.arc(x * 16.15 - 8, y * 16.15 - 8, 1.8, 0, 2 * Math.PI);
+
+                dctx.fill();
+                dctx.closePath();
+                //await sleep(10);
+            }
+
+        }
+    }
+}*/
+isplaying = false;
+redrawMazeTemp(randmaze)
+async function redrawMazeTemp(randmaze) {
+    const response = await fetch("js/mazes.json");
+    const data = await response.json();
+    tab = data[randmaze].layout;
+    j = 0;
+    for (i = 0; i < tab.length; i++) {
+        for (r = 0; r < 2; r++) {
+            if (j == 0) {
+                ctx.moveTo(tab[i][j], tab[i][j + 1]);
+                j = 2;
+            }
+            else {
+                ctx.lineTo(tab[i][j], tab[i][j + 1]);
+                j = 0;
+            }
+
+
+        }
+    }
+    ctx.strokeStyle = "#0000FF";
+    ctx.fill();
+    ctx.stroke();
+}
+
 async function start() {
+    console.log("start");
+    var maze_no = parseInt(localStorage.getItem("mazeno"));
+    console.log(maze_no)
+    redrawMaze(maze_no);
+    isplaying = true;
     tokens = parseInt(localStorage.getItem("tokens"));
     tokens--;
     localStorage.setItem("tokens", tokens.toString());
     var coins = document.getElementById('coins');
-    var grm="TOKENS";
-    if(tokens==1)
-        grm="TOKEN"
-    coins.innerHTML="YOU HAVE: "+tokens+" "+grm+"!";
+    coins.innerHTML = "No. of tokens: " + tokens;
 
     document.getElementById("bg").pause();
+
     mctx.clearRect(0, 0, movCanvas.width, movCanvas.height);
+
     var playbtn = document.getElementById('playbtn');
     playbtn.disabled = true;
     playbtn.className = "nes-btn is-disabled";
 
-
-    var tab = new Array(20);
+    const response = await fetch("js/mazes.json");
+    const data = await response.json();
+    tab = data[maze_no].path;
+    finish = data[maze_no].end;
+    /*var tab = new Array(20);
     for (var i = 0; i < tab.length; i++) {
         tab[i] = new Array(20);
     }
@@ -68,14 +139,31 @@ async function start() {
     tab[17] =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,98,97,96, 0, 0, 0, 0];
     tab[18] =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,101,100,99,0,0, 0, 0, 0, 0];
     tab[19] =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
+*/
     mus();
     spawnDots(18, tab);
-    await sleep (4500);
-    move(tab);
+    var fruit_loc = Math.floor(Math.random() * 65 - 15 + 1) + 15;
+    fruit_tab = decideFruit(data, fruit_loc, maze_no)
+    spawnFruit(18, fruit_tab)
+    await sleep(4500);
+    move(tab, finish);
     x = 9;
     y = 1;
 
+
+    function decideFruit(data, fruit_loc, maze_no) {
+        console.log("decidefruit");
+        var fruit_tab = Array.from(Array(20), () => new Array(20).fill(0));
+        tab = data[maze_no].path;
+        for (i = 1; i < tab.length; i++) {
+            for (j = 1; j < tab[i].length; j++) {
+                if (tab[i][j] == fruit_loc) {
+                    fruit_tab[i][j] = 1;
+                }
+            }
+        }
+        return fruit_tab;
+    }
 
     async function mus() {
         document.getElementById("bootup").play();
@@ -86,7 +174,36 @@ async function start() {
 
     }
 
+    async function redrawMaze(randmaze) {
+        console.log("redrawmaze");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const response = await fetch("js/mazes.json");
+        const data = await response.json();
+        tab = data[randmaze].layout;
+        j = 0;
+        ctx.beginPath();
+        for (i = 0; i < tab.length; i++) {
+            for (r = 0; r < 2; r++) {
+                if (j == 0) {
+                    ctx.moveTo(tab[i][j], tab[i][j + 1]);
+                    j = 2;
+                }
+                else {
+                    ctx.lineTo(tab[i][j], tab[i][j + 1]);
+                    j = 0;
+                }
+
+
+            }
+        }
+        ctx.strokeStyle = "#0000FF";
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+
     async function spawnDots(count, tab) {
+        console.log("pawndots");
         dctx.clearRect(0, 0, dotCanvas.width, dotCanvas.height);
         dctx.fillStyle = "#f6e58d";
         for (var x = 1; x <= count; x++) {
@@ -94,17 +211,31 @@ async function start() {
                 if (tab[y][x] != 0) {
                     dctx.beginPath();
                     dctx.arc(x * 16.15 - 8, y * 16.15 - 8, 1.8, 0, 2 * Math.PI);
-
                     dctx.fill();
                     dctx.closePath();
-                    //await sleep(10);
                 }
 
             }
         }
     }
 
-    async function move(tab) {
+    async function spawnFruit(count, tab) {
+        console.log("spawnfruit");
+        console.log(tab);
+        fctx.clearRect(0, 0, fruitCanvas.width, fruitCanvas.height);
+        for (var x = 1; x <= count; x++) {
+            for (var y = 1; y <= count; y++) {
+                if (tab[y][x] == 1) {
+                    ran = Math.floor(Math.random() * 3);
+                    fctx.drawImage(f_img, 0, (12 * ran), 12, 12, x * 16.15 - 12, y * 16.15 - 12, 10, 10);
+                }
+
+            }
+        }
+    }
+
+    async function move(tab, finish) {
+        console.log("move");
         frame = 1;
         nxt = 2;
         y = 1;
@@ -123,7 +254,7 @@ async function start() {
         tab[y][x] = 0;
         spawnDots(18, tab);
 
-        while (nxt < 102) {
+        while (nxt < finish) {
             mctx.clearRect(0, 0, movCanvas.width, movCanvas.height);
             if (tab[y + 1][x] == nxt) {
                 ymov = y;
@@ -209,7 +340,11 @@ async function start() {
 
                 nxt++;
             }
-
+            if (nxt == fruit_loc + 1) {
+                fctx.clearRect(0, 0, fruitCanvas.width, fruitCanvas.height);
+                await sleep(SLP);
+                document.getElementById("food").play();
+            }
             frame++;
             if (frame == 4)
                 frame = 1;
@@ -227,7 +362,9 @@ async function start() {
 
         await sleep(10500);
         bgm.play();
+        isplaying = false;
 
+        gen();
 
         if (tokens > 0) {
             playbtn.disabled = false;
@@ -235,9 +372,19 @@ async function start() {
         }
     }
 
+    async function gen(){
+        mze = Math.floor(Math.random() * 3);
+        if(parseInt(localStorage.getItem("mazeno")==mze)){
+            gen();
+        }
+        else{
+            localStorage.setItem("mazeno", mze.toString());
+        }
+    }
+
     async function flash() {
         canvas = document.getElementById('canvasMaze');
-        ctx = canvasMaze.getContext('2d');
+        ctx = canvas.getContext('2d');
         for (i = 0; i < 5; i++) {
             await sleep(1000);
             ctx.strokeStyle = "#FFFFFF";
@@ -250,12 +397,52 @@ async function start() {
 
     }
 
-
-
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+}
+var oglasi = new Array(6);
+oglasi[0] = "https://www.youtube.com/watch?v=SJqEfdGzunA";
+oglasi[1] = "https://www.youtube.com/watch?v=i5Amt4ecYBE";
+oglasi[2] = "https://www.youtube.com/watch?v=kmkjGPgMwhw";
+oglasi[3] = "https://www.youtube.com/watch?v=vGJZvhDh5ts";
+oglasi[4] = "https://www.youtube.com/watch?v=cUofaxVXU-g";
+oglasi[5] = "https://www.youtube.com/watch?v=WYZaD3iiWGU";
 
+function watchAd() {
+    var a = oglasi[Math.floor(Math.random() * 6)];
+
+    Swal.fire({
+        title: 'Do you want to see an ad?',
+        text: "You will get one token per ad!",
+        iconHtml: '<img src="img/dollar.png">', // OKAY...
+        customClass: {
+            icon: 'no-border',
+            confirmButton: 'no-border',
+            cancelButton: 'no-border'
+        },
+        showCancelButton: true,
+        confirmButtonColor: '#ffa801',
+        cancelButtonColor: '#485460',
+        confirmButtonText: 'Yes!',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.open(a);
+            var tokens = parseInt(localStorage.getItem("tokens"));
+            tokens++;
+            localStorage.setItem("tokens", tokens.toString());
+
+            var coins = document.getElementById('coins');
+            coins.innerHTML = "No. of tokens: " + tokens;
+
+            var playbtn = document.getElementById('playbtn');
+            if (tokens > 0 && isplaying == false) {
+                playbtn.disabled = false;
+                playbtn.className = "nes-btn is-warning";
+            }
+        }
+    })
 
 }
--- js/swal.js
+++ js/swal.js
@@ -1,53 +1,7 @@
-var oglasi = new Array(6);
-oglasi[0]="https://www.youtube.com/watch?v=SJqEfdGzunA";
-oglasi[1]="https://www.youtube.com/watch?v=i5Amt4ecYBE";
-oglasi[2]="https://www.youtube.com/watch?v=kmkjGPgMwhw";
-oglasi[3]="https://www.youtube.com/watch?v=vGJZvhDh5ts";
-oglasi[4]="https://www.youtube.com/watch?v=cUofaxVXU-g";
-oglasi[5]="https://www.youtube.com/watch?v=WYZaD3iiWGU";
 
-function watchAd(){
-    var a=oglasi[Math.floor(Math.random() * 6)];
-    
-    Swal.fire({
-        title: 'Do you want to see an ad?',
-        text: "You will get one token per ad!",
-        iconHtml: '<img src="img/dollar.png">', // OKAY...
-        customClass: {
-            icon: 'no-border',
-            confirmButton: 'no-border',
-            cancelButton: 'no-border'
-          },
-        showCancelButton: true,
-        confirmButtonColor: '#ffa801',
-        cancelButtonColor: '#485460',
-        confirmButtonText: 'Yes!',
-        cancelButtonText: 'No',
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.open(a);
-          var tkn=parseInt(localStorage.getItem("tokens"));
-          tkn++;
-          localStorage.setItem("tokens", tkn.toString());
-
-          var coins=document.getElementById('coins');
-          var grm="TOKENS";
-          if(tkn==1)
-            grm="TOKEN"
-          coins.innerHTML="YOU HAVE: "+tkn+" "+grm+"!";
-
-          var playbtn = document.getElementById('playbtn');
-          if(tkn>0){
-            playbtn.disabled =false;
-            playbtn.className = "nes-btn is-warning";
-        }
-        }
-      })
-
-}
 function about(){
     Swal.fire({
-        icon: 'info',
+        iconHtml: 'C',
         title:'About',
         text: 'Website made by Kristjan Brataševec, 4. RA, 2022',
         confirmButtonColor: '#485460',
